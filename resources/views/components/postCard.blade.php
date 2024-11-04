d4f0cb6ac862edf326cfb15298ea80358838e4a1
Ubuntu22SSD2 Jaycee <jayceealtamia@gmail.com>
2024-11-04 18:43:09 +0800

    update author

--- resources/views/components/postCard.blade.php
+++ resources/views/components/postCard.blade.php
@@ -3,7 +3,9 @@
 <div class="bg-white p-6 rounded-lg shadow-lg ">
     <h4 class="text-xl font-bold text-gray-800 mb-2">{{$post->title}}</h4>
         <div class="text-gray-600 mb-4"><span class="text-gray-800 text-sm ">Posted {{$post->created_at->diffForHumans()}} by:</span> 
-            <a href="#" class="text-blue-500 hover:text-blue-700">User</a>
+            <a href="#" class="text-blue-500 hover:text-blue-700">
+                {{$post->user->username}}
+            </a>
         </div>
         <p class="text-gray-600">{{Str::words($post->body, 15)}}</p>
  </div>
